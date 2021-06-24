3cce77ba2596f42fa5be4d39cecb73ae7aff609e
arihant jain <jain.ari2000@gmail.com>
2021-06-24 11:37:46 +0530

    separate files for dev and production

---
+++ config/prod.js
@@ -0,0 +1,11 @@
+//prod.js production keys
+module.exports={
+    googleClientID: process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret:process.env.GOOGLE_CLIENT_SECRET,
+    spotifyClientID:process.env.SPOTIFY_CLIENT_ID,
+    spotifyClientSecret:process.env.SPOTIFY_CLIENT_SECRET,
+    mongoURI:process.env.MONGO_URI,
+    cookieKey:process.env.COOKIE_KEY
+}
+
+//dUN6E2LwEW178DXZ
